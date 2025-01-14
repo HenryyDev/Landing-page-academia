17593ef85e22badb5616a9aacf151d1ee7ce6fae
henry4i20 <henrimito1305@gmail.com>
2025-01-14 18:42:36 -0300

    feat: adiciona media query para section03 e botao saiba mais

--- frontEnd/src/Components/Section03.jsx
+++ frontEnd/src/Components/Section03.jsx
@@ -7,32 +7,47 @@ import iconVerificado from "../assets/verifica.png";
 
 export default function Section03() {
   return (
-    <div className="container container-section03 my-5 d-flex">
-      <div className="esquerda-section03">
-        <img className="img-fluid img-foto01" src={foto01} alt="Imagem 1" />
-        <img className="img-fluid img-foto02 " src={foto02} alt="Imagem 2" />
-        <img className="img-fluid img-foto03" src={foto03} alt="Imagem 3" />
+    <>
+      <div className="container container-section03 my-5 d-flex">
+        <div className="esquerda-section03">
+          <img className="img-fluid img-foto01" src={foto01} alt="Imagem 1" />
+          <img className="img-fluid img-foto02 " src={foto02} alt="Imagem 2" />
+          <img className="img-fluid img-foto03" src={foto03} alt="Imagem 3" />
+        </div>
+        <div className="direita-section03 mb-5">
+          <h1 className="txt-title">
+            Transforme seu corpo e alcance seu melhor com nosso plano de
+            condicionamento físico.
+          </h1>
+          <ul>
+            <li>
+              <img src={iconVerificado} alt="" width={"24px"} />
+              <span style={{ color: "white" }}>Construa músculos</span>
+            </li>
+            <li>
+              <img src={iconVerificado} alt="" width={"24px"} />
+              <span style={{ color: "white" }}>Transforme-se por completo</span>
+            </li>
+            <li>
+              <img src={iconVerificado} alt="" width={"24px"} />
+              <span style={{ color: "white" }}>Liberte seu potencial</span>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="direita-section03 mb-5">
-        <h1 className="txt-title">
-          Transforme seu corpo e alcance seu melhor com nosso plano de
-          condicionamento físico.
-        </h1>
-        <ul>
-          <li>
-            <img src={iconVerificado} alt="" width={"24px"} />
-            <span style={{ color: "white" }}>Construa músculos</span>
-          </li>
-          <li>
-            <img src={iconVerificado} alt="" width={"24px"} />
-            <span style={{ color: "white" }}>Transforme-se por completo</span>
-          </li>
-          <li>
-            <img src={iconVerificado} alt="" width={"24px"} />
-            <span style={{ color: "white" }}>Liberte seu potencial</span>
-          </li>
-        </ul>
+      <div className="container container-card-section03 my-5">
+        <div>
+          <h3 className="txt-title">
+            suporte personalizado e uma comunidade motivadora para alcançar seus
+            objetivos!
+          </h3>
+        </div>
+        <div>
+          <a href="#" className="btn btn-primary mx-5">
+            Saiba mais
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
